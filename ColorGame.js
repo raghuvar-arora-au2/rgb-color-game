cd10c2eab8c05262d9d8de625c175b733002dc89
raghuvar <raghuvararora@gmail.com>
2017-05-27 19:57:50 +0530

    minor additions

--- ColorGame.js
+++ ColorGame.js
@@ -8,8 +8,18 @@ var b=random(0,255);
 for(i=0; i<6;i++){
 	var sqr=document.getElementsByClassName("square")[i];
 	sqr.style.backgroundColor=("rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")");
-
+	sqr.addEventListener("click", function(){
+		this.style.visibility="hidden";
+	});
 }
 
 var correctbox=document.getElementsByClassName("square")[random(0,5)];
 var correctrgb=correctbox.style.backgroundColor;
+
+correctbox.addEventListener("click", function(){
+	this.style.visibility="visible";
+})
+
+
+var h2=document.querySelector("h2");
+h2.textContent=correctrgb;
